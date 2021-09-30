bc420c09d807021812acfa4bfd69c9750da3ef57
Duvivier <duvivier.clem@gmail.com>
2021-09-30 12:45:40 +0200

    :construction: add some css and js
    some style and js

--- tuto-klem.js
+++ tuto-klem.js
@@ -104,3 +104,15 @@ let hello = ' say hello !';
 hello;
 let greeting = name + hello;
 greeting;
+
+const button = document.querySelector('button');
+
+button.onclick = function() {
+
+    let name = prompt('What\'s your name ?');
+    alert('hi ' + name + ', nice to see you here !');
+
+}
+let myName = 'clément';
+let age = 20;
+let character = {name : 'anakin', rank : 'jedi knight'};
