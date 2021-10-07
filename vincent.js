e1b26f89178633f87f054093f5e822fc2467785f
Vincent Michel <Vincent.michel@edu.devinci.Fr>
2021-10-07 10:02:12 +0200

    :lipstick: Adding Prism !
    .

--- vincent.js
+++ vincent.js
@@ -1,11 +1,11 @@
 const btn = document.querySelector('.Arrow');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', () => {//when we click on the arrow, this append:
 
     window.scrollTo({
-        top: 800,
+        top: 800,//We scroll the page to downPage
         left: 0,
-        behavior: "smooth",
+        behavior: "smooth",//and we scroll with smooth, better for your eyes
     })
 
 });
