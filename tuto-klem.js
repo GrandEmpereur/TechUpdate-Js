c3eee1eb3f869578d386af35cd0fd63a3b9acc65
Duvivier <duvivier.clem@gmail.com>
2021-10-07 11:15:23 +0200

    :lipstick: update flexbox
    change some placement with flexbox for more clarity

--- tuto-klem.js
+++ tuto-klem.js
@@ -59,9 +59,9 @@ function checkGuess() { //function
         }
     }
 
-guessCount++;
-guessField.value = '';
-guessField.focus();
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
 }
 guessSubmit.addEventListener('click', checkGuess);
 
